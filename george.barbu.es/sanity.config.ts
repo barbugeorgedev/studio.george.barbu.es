110ec5616f972dce08206e9f8bf074ae003f0d96
george.barbu <barbu.george89@gmail.com>
2025-02-09 11:15:40 +0100

    add Dashboar to have access to GraphQL url

--- george.barbu.es/sanity.config.ts
+++ george.barbu.es/sanity.config.ts
@@ -2,9 +2,8 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
-
-
-import { vercelDeployTool } from 'sanity-plugin-vercel-deploy'
+import {dashboardTool, projectUsersWidget, projectInfoWidget} from '@sanity/dashboard'
+import {vercelDeployTool} from 'sanity-plugin-vercel-deploy'
 
 export default defineConfig({
   name: 'default',
@@ -13,7 +12,14 @@ export default defineConfig({
   projectId: 'bet7jatc',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool(), vercelDeployTool()],
+  plugins: [
+    structureTool(),
+    visionTool(),
+    vercelDeployTool(),
+    dashboardTool({
+      widgets: [projectInfoWidget(), projectUsersWidget()],
+    }),
+  ],
 
   schema: {
     types: schemaTypes,
