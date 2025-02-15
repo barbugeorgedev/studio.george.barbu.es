afcb6b59ea7750d380f72c0358180231603cfde7
george.barbu <barbu.george89@gmail.com>
2025-02-15 18:17:18 +0100

    add payment options

--- apps.andaluciada.es/schemaTypes/reservation.ts
+++ apps.andaluciada.es/schemaTypes/reservation.ts
@@ -15,6 +15,12 @@ export default defineType({
       options: {list: ['reserved', 'paid']},
     }),
     defineField({
+      name: 'paymentMethod',
+      type: 'string',
+      title: 'Payment Method',
+      options: {list: ['bank transfer', 'credit card', 'cash']},
+    }),
+    defineField({
       name: 'reservedAt',
       type: 'datetime',
       title: 'Reserved At',
@@ -26,12 +32,13 @@ export default defineType({
       title: 'userPhone',
       status: 'status',
       number: 'number',
+      paymentMethod: 'paymentMethod',
     },
-    prepare({title, status, number}) {
+    prepare({title, status, number, paymentMethod}) {
       const numberList = number ? `[ ${number.join(', ')} ]` : 'No numbers'
       return {
         title,
-        subtitle: `${status} - ${numberList}`,
+        subtitle: `${status} - ${paymentMethod} - ${numberList}`,
       }
     },
   },
