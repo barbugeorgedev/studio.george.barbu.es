112c3c05838f47c1ce90b0c2ed3cf7bc60bd4921
george.barbu <barbu.george89@gmail.com>
2025-02-20 11:02:41 +0100

    add contact form

--- losdoritos.es/schemaTypes/index.ts
+++ losdoritos.es/schemaTypes/index.ts
@@ -2,5 +2,6 @@ import slide from './slide'
 import settings from './settings'
 import wheel from './wheel'
 import ad from './ad'
+import contact from './contactForm'
 
-export const schemaTypes = [wheel, settings, slide, ad]
+export const schemaTypes = [wheel, settings, slide, ad, contact]
