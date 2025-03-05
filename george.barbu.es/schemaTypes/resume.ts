c2780bac630f8ef112d782a7dc41e30ad583beb0
george.barbu <barbu.george89@gmail.com>
2025-03-05 19:14:01 +0100

    validation for only 1 homepage

--- george.barbu.es/schemaTypes/resume.ts
+++ george.barbu.es/schemaTypes/resume.ts
@@ -1,4 +1,12 @@
 import {defineType} from 'sanity'
+import {createClient} from '@sanity/client'
+
+// Use the new `createClient` instead of the deprecated `sanityClient`
+const client = createClient({
+  useCdn: true,
+  projectId: 'bet7jatc',
+  dataset: 'production',
+})
 
 export default defineType({
   name: 'resume',
@@ -9,6 +17,16 @@ export default defineType({
       name: 'cvpurpose',
       title: 'CV Name',
       type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'cvpurpose',
+        maxLength: 96,
+      },
+      validation: (Rule) => Rule.required(),
     },
     {
       name: 'settings',
@@ -55,15 +73,31 @@ export default defineType({
       type: 'array',
       of: [{type: 'skillsSection'}],
     },
-    {name: 'experienceSection', title: 'Experience Section', type: 'experienceSection'},
+    {
+      name: 'experienceSection',
+      title: 'Experience Section',
+      type: 'experienceSection',
+    },
     {
       name: 'earlyCareerExperienceSection',
       title: 'Early Career Experience Section',
       type: 'earlyCareerExperienceSection',
     },
-    {name: 'ngoExperienceSection', title: 'NGO Experience Section', type: 'ngoExperienceSection'},
-    {name: 'educationSection', title: 'Education Section', type: 'educationSection'},
-    {name: 'contactSection', title: 'Contact Section', type: 'contactSection'},
+    {
+      name: 'ngoExperienceSection',
+      title: 'NGO Experience Section',
+      type: 'ngoExperienceSection',
+    },
+    {
+      name: 'educationSection',
+      title: 'Education Section',
+      type: 'educationSection',
+    },
+    {
+      name: 'contactSection',
+      title: 'Contact Section',
+      type: 'contactSection',
+    },
     {
       name: 'social',
       title: 'Social Profiles',
@@ -76,6 +110,31 @@ export default defineType({
       name: 'homepage',
       type: 'boolean',
       title: 'Set as Homepage',
+      validation: (Rule) =>
+        Rule.custom(async (value, context) => {
+          if (value) {
+            const documentId = context.document?._id
+
+            if (documentId) {
+              // Query to check if another document with homepage = true exists, excluding the current one
+              const existingHomepages = await client.fetch(
+                `
+              *[_type == "resume" && homepage == true && _id != $documentId] {
+                _id
+              }
+            `,
+                {documentId},
+              )
+              // If there is another resume set as homepage, show an error
+              if (existingHomepages.length > 0) {
+                return 'Only one resume can be set as the homepage'
+              }
+            }
+          }
+
+          // Allow if there's no conflict
+          return true
+        }),
     },
   ],
   preview: {
